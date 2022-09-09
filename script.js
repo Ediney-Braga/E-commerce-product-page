b39ebbf96784e55519cf419c7bc11e2095a7c1c8
Ed <contatoedineybraga@gmail.com>
2022-09-09 13:46:57 -0300

    new js events

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const hiddenCart = document.querySelector("#hiddenCartCashier");
 cart.addEventListener("click", () => {
     hiddenCart.classList.toggle("open")
     if(hiddenCart.classList.contains ("open")){
-        hiddenCart.style.display = "block";     
+        hiddenCart.style.display = "block";  
+        addNotivication();
     } else {
         hiddenCart.style.display = "none";
     }
@@ -13,6 +14,25 @@ cart.addEventListener("click", () => {
 
 //  fim parte-- mostrar conteudo do carrinho
 
+// calcula a quantia a ser paga de acordo com a quantidade de itens adicionados ao carrinho ---- e ---- mostra uma notificação de itens add ao carrinho.
+const addToCart = document.querySelector("#adiciona");
+addToCart.addEventListener("click", addNotivication)
+
+function addNotivication(){
+    let aPagar = (125.00 * amount.value).toFixed(2);
+    let vezesAPagar = document.querySelector("#vezesAPagar");
+    vezesAPagar.innerHTML = `<p id="vezesAPagar">$125.00 x ${amount.value}<small>$${aPagar}</small></p>`
+
+    let cartNotification = document.querySelector("#cartNotification");
+
+    if(amount.value > 0 && !hiddenCart.classList.contains("open")){
+        cartNotification.style.display = "block";
+    } else {
+        cartNotification.style.display = "none";
+    }
+    cartNotification.innerHTML =`<p>${amount.value}</p>`
+}
+//-----------------------------------------------
 
 // modifica a quantidade do produto a ser adcionada ao carrinho
 const up = document.querySelector("#up");
@@ -33,23 +53,6 @@ function mostrarAmount (element) {
 }
 
 
-// calcula a quantia a ser paga de acordo com a quantidade de itens adicionados ao carrinho ---- e ---- mostra uma notificação de itens add ao carrinho.
-const addToCart = document.querySelector("#adiciona");
-addToCart.addEventListener("click", () => {
-
-    let aPagar = (125.00 * amount.value).toFixed(2);
-    let vezesAPagar = document.querySelector("#vezesAPagar");
-    vezesAPagar.innerHTML = `<p id="vezesAPagar">$125.00 x ${amount.value}<small>$${aPagar}</small></p>`
-
-    let cartNotification = document.querySelector("#cartNotification");
-    if(amount.value > 0){
-        cartNotification.style.display = "block";
-    } else {
-        cartNotification.style.display = "none";
-    }
-    cartNotification.innerHTML =`<p>${amount.value}</p>`
-})
-//-----------------------------------------------
 
 // ------------  FUNÇÃO DELETE AINDA NÃO ESTA FUNCINAL----------
 // let del = document.querySelector("#del");
@@ -61,14 +64,66 @@ addToCart.addEventListener("click", () => {
 // })
 
 
+// --------------- VIEW FULL SCREEN PICTURE --------------------
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const close = document.querySelector("#close");
+
+const pictureFullScreen = document.querySelector("#viewFullScreenPicture img");
+    
+let boxFull = document.querySelector("#viewFullScreenPicture");
+let num = 1;
+left.addEventListener("click", slidePicture);
+right.addEventListener("click", slidePicture);
+
+close.addEventListener("click", () => {
+    boxFull.style.visibility = "hidden";
+})
+
+function slidePicture (event) {
+    let picFSLocal = pictureFullScreen.src;
+    let eventId = event.target.id;
+    
+    if(eventId == "right"){
+        if(num == 4){
+            num = 0;
+        }
+        num += 1;
+        pictureFullScreen.src = `./images/image-product-${num}.jpg`
+        console.log(num);
+    } else {
+        if(num == 1){
+            num = 5;
+        }
+        num -= 1;
+        pictureFullScreen.src = `./images/image-product-${num}.jpg` 
+        console.log(num);
+    }
+}
+
+
+// -----------  SELECIONANDO A IMAGEM QUE QUER VER -------------
 const amostraPictures = document.querySelectorAll("#amostraPictures img");
+let selectedPicture = document.querySelector("#selectedPicture img");
+
+selectedPicture.addEventListener("click", ()=>{
+    boxFull.style.visibility = "visible";
+})
+
 amostraPictures.forEach((picture)=>{
     picture.addEventListener("click", pictureSelected);
     
 })
 
+function opacityPictures(){
+    amostraPictures.forEach((picture) => {
+        picture.classList.remove("opacity");
+    })
+}
+
 function pictureSelected (picture) {
-    let selectedPicture = document.querySelector("#selectedPicture img");
+    opacityPictures();
     let newPictureSelected = picture.target.src.replace("-thumbnail.jpg",".jpg");
     selectedPicture.src = newPictureSelected;
+    picture.target.classList.add("opacity");
 }
